a6dd6c69781323739176f70c9839f6812237d7fd
13537584001 <271357630@qq.com>
2026-04-17 16:34:46 +0800

    fix():

--- custom_components/aam_home/switch.py
+++ custom_components/aam_home/switch.py
@@ -45,6 +45,8 @@ class AamSwitchEntity(IoTPropertyEntity, SwitchEntity):
     @property
     def is_on(self) -> bool:
         """开/关 状态."""
+        if self._value is None:
+            return False
         return self._value.get("State", 0) == 1
 
     async def async_turn_on(self, **kwargs: Any) -> None:
