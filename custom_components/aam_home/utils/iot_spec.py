6fc5458a3970d1d31abb608beb1283b6b460d64b
13537584001 <271357630@qq.com>
2026-04-27 17:57:01 +0800

    fix():

--- custom_components/aam_home/utils/iot_spec.py
+++ custom_components/aam_home/utils/iot_spec.py
@@ -294,7 +294,7 @@ class IoTSpecParser:
                 details = property_.get('details', {})
                 for detail in details:
                     detail['description'] = detail['paramName']
-                    detail['name'] = detail['paramName']
+                    detail['name'] = detail['aamParam']
 
                     # 操作(指令下发设置属性值)
                     if prop_info['propType'] == 2:
